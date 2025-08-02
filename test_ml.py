f2c8916f7576845d1b23cdaa4a910761383269e7
Mark <mark.inabnit@gmail.com>
2025-08-02 15:08:48 -0500

    0.5.0 - Completed Unit Tests.

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,30 @@
 import pytest
-# TODO: add necessary import
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
+@pytest.fixture(scope="session")
+def dataset():
+    project_path = './'
+    data_path = os.path.join(project_path, "data", "census.csv")
+    data = pd.read_csv(data_path)
+    return data
+
+def test_DataSetType(dataset):
+    #Test that dataset is a pandas dataframe
+    assert isinstance(dataset, pd.DataFrame)
+    pass
+
+def test_NumberOfColumns(dataset):
+    #Test to check dataset has the expected number of columns
+    assert dataset.shape[1] == 15
+    pass
+
+def test_TrainingTestType(dataset):
+    #Test check the training sets retains the correct dtype after splitting
+    train, test = train_test_split(dataset, test_size = .10)
+    assert isinstance(train, pd.DataFrame)
     pass
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
 
-
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
