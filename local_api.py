94051901fdee9571b0705b998b73b6aa6001eb63
Mark <mark.inabnit@gmail.com>
2025-08-04 17:37:21 -0500

    0.6.0 - Completed Local API testing

--- local_api.py
+++ local_api.py
@@ -2,13 +2,10 @@ import json
 
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+r = requests.get('http://127.0.0.1:8000') # Your code here
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+print( "Status Code:", r.status_code)
+print(r.text)
 
 
 
@@ -29,10 +26,7 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post("http://127.0.0.1:8000/data/", json = data) # Your code here
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+print("Status Code:", r.status_code)
+print(r.json())
